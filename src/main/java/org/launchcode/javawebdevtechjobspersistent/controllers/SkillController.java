51978e8b71afcc4a34b65ab47e4172f63ef2573f
Matthew Clockel <35537679+Matt87c@users.noreply.github.com>
2020-09-01 21:00:25 -0400

    Update SkillController.java

--- src/main/java/org/launchcode/javawebdevtechjobspersistent/controllers/SkillController.java
+++ src/main/java/org/launchcode/javawebdevtechjobspersistent/controllers/SkillController.java
@@ -27,16 +27,16 @@ public class SkillController {
     @Autowired
     private EmployerRepository employerRepository;
 
+    @RequestMapping("")
+    public String skillsViewAll(Model model) {
+        model.addAttribute("skills", skillRepository.findAll());
+        return "skills/index";
+    }
+    
     @GetMapping("add")
     public String displayAddSkillForm(Model model) {
         model.addAttribute(new Skill());
         return "skills/add";
-    }
-
-    @GetMapping("")
-    public String skillsViewAll(Model model) {
-        model.addAttribute("skills", skillRepository.findAll());
-        return "skills/index";
     }
 
     @PostMapping("add")
