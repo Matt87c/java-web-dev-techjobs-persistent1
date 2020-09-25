9b6dac2ee085f6d510a91dd3cafc13a54b62a840
Matthew Clockel <35537679+Matt87c@users.noreply.github.com>
2020-09-25 08:27:06 -0400

    Update queries.sql

--- queries.sql
+++ queries.sql
@@ -1,13 +1,12 @@
 ## Part 1: Test it with SQL
-SELECT * FROM job;
+show columns FROM job;
 ## Part 2: Test it with SQL
 SELECT name FROM employer
 WHERE location = 'St Louis City';
 ## Part 3: Test it with SQL
 DROP TABLE job;
 ## Part 4: Test it with SQL
-SELECT name, description
+SELECT DISTINCT name, description
 FROM skill
-INNER JOIN job ON skill
-WHERE skill IS NOT NULL
+INNER JOIN job_skills ON skill.id = job_skills.skills_id
 ORDER BY name;
